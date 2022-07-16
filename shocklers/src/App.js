e09b2da96e25af46ea1f67c5f198f4ce3c83cb2b
Ferrois <ferrois2005@gmail.com>
2022-07-17 01:15:12 +0800

    First Commit

---
+++ shocklers/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Link, BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
